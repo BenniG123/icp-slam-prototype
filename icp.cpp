9ff834c42c7386a9b06a7740958c3cd174c3a2d6
Ben Williams <w.ben70@gmail.com>
2017-03-06 14:43:57 -0600

    ICP and point alignment improvement

--- icp.cpp
+++ icp.cpp
@@ -48,10 +48,10 @@ namespace icp {
 		/* double x = -2 * 3.14 / 180;
 		float d[3][3] = {{1, 0, 0}, {0, (float) cos(x), (float) -sin(x)}, {0, (float) sin(x),(float) cos(x)}};
 		cv::Mat a(3, 3, CV_32FC1, &d);
-
+		
 		dataCloud.rotate(a);
 		*/
-
+		
 		findNearestNeighborAssociations(dataCloud, previousCloud, errors, associations);
 		
 		// dataCloud.points = B;
@@ -63,25 +63,11 @@ namespace icp {
 
 			showPointCloud(dataCloud, depthWindow, cv::viz::Color().green(), "Data");
 			showPointCloud(previousCloud, depthWindow, cv::viz::Color().yellow(), "Previous");
-			// Set our viewpoint
-			// cv::Point3f c = dataCloud.center;
-			// cv::Vec3f v(c.x, c.y, c.z);
-			// depthWindow.setViewerPose(cv::viz::makeCameraPose(cv::Vec3f(0, 0, 20), v, cv::Vec3f(0, 1, 0)));
 
 			depthWindow.spinOnce(33, true);
 
-			// cv::waitKey(0);
-			// Transform our data to a form that is easily solvable by SVD
-
 			cv::Mat dataMat = dataCloud.centered_matrix();
 			cv::Mat previousMat = previousCloud.centered_matrix();
-			
-			// std::cout << dataCloud.center << std::endl;
-			// cv::waitKey(0);
-			// std::cout << P << std::endl;
-			// cv::waitKey(0);
-			// std::cout << Q << std::endl;
-			// cv::waitKey(0);
 
 			// Make sure the data is the same size - proper alignment
 			if (dataMat.size().area() > previousMat.size().area()) {
@@ -96,15 +82,16 @@ namespace icp {
 			// Perform SVD
 			cv::SVD svd(M);
 
-			// std::cout << "Cross Covariance" << std::endl << M << std::endl;
-			// cv::waitKey(0);
-
 			// Rotational Matrix
 			cv::Mat R =  svd.vt.t() * svd.u.t();
 
-			// Testing rotation
-			// float data[3][3] = {{0, -1, 0}, {1, 0, 0}, {0,0,1}}; 
-			// R = cv::Mat(3, 3, CV_32FC1, &data);
+			if (cv::determinant(R) < 0) {
+				std::cout << "Reflection Detected" << std::endl;
+				cv::Mat Vt = svd.vt;
+				std::cout << Vt << std::endl;
+				R = Vt.t() * svd.u.t();
+				cv::waitKey(0);
+			}
 			
 			if (i == 0) {
 				R.copyTo(rigidTransformation(cv::Rect(0, 0, 3, 3)));
@@ -114,17 +101,7 @@ namespace icp {
 				Rp.copyTo(rigidTransformation(cv::Rect(0, 0, 3, 3)));
 			}
 
-
-			// std::cout << "Rotation" << std::endl << R << std::endl;
-			// cv::waitKey(0);
-
-			// Transform the new data
-			// R = R.inv();
 			previousCloud.rotate(R);
-
-			// showPointCloud(previousCloud, depthWindow, cv::viz::Color().red(), "Rotated Previous");
-			// depthWindow.spinOnce(1000, true);
-
 
 			// Find nearest neighber associations
 			findNearestNeighborAssociations(dataCloud, previousCloud, errors, associations);
@@ -203,7 +180,7 @@ namespace icp {
 
 		// std::cout << previous.points.size() << std::endl;
 
-		/* 
+		
 		data.points.clear();
 		previous.points.clear();
 
@@ -218,8 +195,6 @@ namespace icp {
 			// std::cout << "Assocation: " << (*it1).first << " " << (*it1).second << std::endl; 
 			it1++;
 		}
-		*/
-
 	}
 
 	// Bottlenecking function - Hardware acceleration candidate
@@ -276,95 +251,4 @@ namespace icp {
 
 		return error_sum;
 	}
-
-	/* High level ICP Code
-	pair.clear(); dists.clear();
-		    double dist = flann_knn(destination, X, pair, dists);
-		 
-		    if(lastDist <= dist) {
-		        X = lastGood;
-		        break;  //converged?
-		    }
-		    
-		    lastDist = dist;
-		    X.copyTo(lastGood);
-		 
-		    std::cout << "distance: " << std::dist << std::endl;
-		 
-		    cv::Mat X_bar(X.size(),X.type());
-		    for(int i=0;i<X.rows;i++) {
-		        cv::Point p = destination.at<cv::Point>(pair[i],0);
-		        X_bar.at<cv::Point>(i,0) = p;
-		    }
-		 
-		    ShowQuery(destination,X,X_bar);
-		 
-		    X = X.reshape(2);
-		    X_bar = X_bar.reshape(2);
-		    findBestReansformSVD(X,X_bar);
-		    X = X.reshape(1); // back to 1-channel
-		}
-		*/
-
-	/* float flann_knn(Mat& m_destinations, Mat& m_object, vector<int>& ptpairs, vector<float>& dists = vector<float>()) {
-	    // find nearest neighbors using FLANN
-	    cv::Mat m_indices(m_object.rows, 1, CV_32S);
-	    cv::Mat m_dists(m_object.rows, 1, CV_32F);
-	 
-	    Mat dest_32f; m_destinations.convertTo(dest_32f,CV_32FC2);
-	    Mat obj_32f; m_object.convertTo(obj_32f,CV_32FC2);
-	 
-	    assert(dest_32f.type() == CV_32F);
-	 
-	    cv::flann::Index flann_index(dest_32f, cv::flann::KDTreeIndexParams(2));  // using 2 randomized kdtrees
-	    flann_index.knnSearch(obj_32f, m_indices, m_dists, 1, cv::flann::SearchParams(64) ); 
-	 
-	    int* indices_ptr = m_indices.ptr<int>(0);
-	    //float* dists_ptr = m_dists.ptr<float>(0);
-	    for (int i=0;i<m_indices.rows;++i) {
-	        ptpairs.push_back(indices_ptr[i]);
-	    }
-	 
-	    dists.resize(m_dists.rows);
-	    m_dists.copyTo(Mat(dists));
-	 
-	    return cv::sum(m_dists)[0];
-	}
-
-	void findBestReansformSVD(Mat& _m, Mat& _d) {
-	    Mat m; _m.convertTo(m,CV_32F);
-	    Mat d; _d.convertTo(d,CV_32F);
-	 
-	    Scalar d_bar = mean(d);
-	    Scalar m_bar = mean(m);
-	    Mat mc = m - m_bar;
-	    Mat dc = d - d_bar;
-	 
-	    mc = mc.reshape(1); dc = dc.reshape(1);
-	     
-	    Mat H(2,2,CV_32FC1);
-	    for(int i=0;i<mc.rows;i++) {
-	        Mat mci = mc(Range(i,i+1),Range(0,2));
-	        Mat dci = dc(Range(i,i+1),Range(0,2));
-	        H = H + mci.t() * dci;
-	    }
-	 
-	    cv::SVD svd(H);
-	 
-	    Mat R = svd.vt.t() * svd.u.t();
-	    double det_R = cv::determinant(R);
-	    if(abs(det_R + 1.0) < 0.0001) {
-	        float _tmp[4] = {1,0,0,cv::determinant(svd.vt*svd.u)};
-	        R = svd.u * Mat(2,2,CV_32FC1,_tmp) * svd.vt;
-	    }
-
-	    float* _R = R.ptr<float>(0);
-	    Scalar T(d_bar[0] - (m_bar[0]*_R[0] + m_bar[1]*_R[1]),d_bar[1] - (m_bar[0]*_R[2] + m_bar[1]*_R[3]));
-	 
-	    m = m.reshape(1);
-	    m = m * R;
-	    m = m.reshape(2);
-	    m = m + T;// + m_bar;
-	    m.convertTo(_m,CV_32S);
-	} */
 }
